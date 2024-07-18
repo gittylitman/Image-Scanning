c33facc85d5cdd2033f4e2319fafe3f5f2274851
GitiL <GitiL@skyvar.co.il>
2024-07-18 14:58:36 +0300

    try to upload a image to the azure container regstry

--- image-scanning/app.py
+++ image-scanning/app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    print(request.get())
-    return 'hello!'
+    data = request.json
+    return data.get('name')
 
 
 if __name__ == "__main__":
-    print("starting")
     serve(app, host="0.0.0.0", port=8080)
