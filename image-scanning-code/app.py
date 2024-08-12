f91048002604eee93bb8a5666cae58cafb098518
GitiL <GitiL@skyvar.co.il>
2024-08-12 15:07:01 +0300

    try from begin

--- image-scanning-code/app.py
+++ image-scanning-code/app.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    response = request.get_json()
-    run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
-    return response
+    # response = request.get_json()
+    # run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
+    run_resource_graph_query("sha256:6b37597ee8a5601c23762453be959cc43c0689df6efd67f0c2f07db7636c9281","getmy-num", "1-1-2003")
+    # return response
 
 
 
 if __name__ == "__main__":
+    send_to_image_scanning()
     serve(app, host="0.0.0.0", port=8080)
