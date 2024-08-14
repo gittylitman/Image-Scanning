91329b7564001b34eaf5a22a593e581b4c847f15
GitiL <GitiL@skyvar.co.il>
2024-08-14 12:00:47 +0300

    try

--- image_scanning_code/app.py
+++ image_scanning_code/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from waitress import serve
-from project.image_scanning import run_resource_graph_query
+from project.image_scanning import run_resource_graph_query, send_message_to_rabbitmq
 
 
 app = Flask(__name__)
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    # response = request.get_json()
+    response = request.get_json()
     # run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
-    run_resource_graph_query("sha256:01bc7b5bf458832823d2c7334aeefa127a81296b0506d24852466781f760ee9c","services/storage_account/func_check_storage", "1-1-2003")
-    # return response
+    # run_resource_graph_query("sha256:01bc7b5bf458832823d2c7334aeefa127a81296b0506d24852466781f760ee9c","services/storage_account/func_check_storage", "1-1-2003")
+    send_message_to_rabbitmq("tryyyy","4.156.100.2","logs","admin","admin")
+    return response
 
 
 
