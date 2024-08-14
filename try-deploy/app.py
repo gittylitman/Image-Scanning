15c8112b1d8c9466d73961b60f1488ffa983f5e1
GitiL <GitiL@skyvar.co.il>
2024-08-14 13:03:25 +0300

    try deploy try

---
+++ try-deploy/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from waitress import serve
+
+
+app = Flask(__name__)
+
+
+@app.route("/image_push_acr", methods=["POST"])
+def send_to_image_scanning():
+    response = request.get_json()
+    # # run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
+    # # run_resource_graph_query("sha256:01bc7b5bf458832823d2c7334aeefa127a81296b0506d24852466781f760ee9c","services/storage_account/func_check_storage", "1-1-2003")
+    # send_message_to_rabbitmq("tryyyy","4.156.100.2","logs","admin","admin")
+    return "hellooooo"
+
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8080)
