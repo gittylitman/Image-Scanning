656dda39aa3e7cd5b9351333c2f0f26a66994754
GitiL <GitiL@skyvar.co.il>
2024-08-15 10:54:39 +0300

    upload with other workflow

--- image-scanning-code/app.py
+++ image-scanning-code/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from waitress import serve
+import time
 from project.image_scanning import run_resource_graph_query, send_message_to_rabbitmq
 
 
@@ -9,8 +10,8 @@ app = Flask(__name__)
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
     response = request.get_json()
+    time.sleep(300)
     run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
-    # run_resource_graph_query("sha256:01bc7b5bf458832823d2c7334aeefa127a81296b0506d24852466781f760ee9c","services/storage_account/func_check_storage", "1-1-2003")
     return response
 
 
--- try-deploy/app.py
+++ try-deploy/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from waitress import serve
+import logging
+import pika
 
 
 app = Flask(__name__)
@@ -7,7 +9,19 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    response = request.get_json()
+    # def send_message_to_rabbitmq(message, host, queue, username, password):
+    try:
+        credentials = pika.PlainCredentials("admin", "admin")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="4.156.100.2", credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue="logs")
+        channel.basic_publish(exchange='',
+                              routing_key="logs",
+                              body="hello rabbit:)")
+        connection.close()
+    except Exception as e:
+        return "Error sending message to RabbitMQ: {e}"
+
     # # run_resource_graph_query(response["target"]["digest"],response["target"]["repository"], response["timestamp"])
     # # run_resource_graph_query("sha256:01bc7b5bf458832823d2c7334aeefa127a81296b0506d24852466781f760ee9c","services/storage_account/func_check_storage", "1-1-2003")
     # send_message_to_rabbitmq("tryyyy","4.156.100.2","logs","admin","admin")
