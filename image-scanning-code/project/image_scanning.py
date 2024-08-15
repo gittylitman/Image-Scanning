4d96f4f785655ca6a11f6ba26af36ae05f31c12f
GitiL <GitiL@skyvar.co.il>
2024-08-15 13:36:01 +0300

    upload with other workflow

--- image-scanning-code/project/image_scanning.py
+++ image-scanning-code/project/image_scanning.py
@@ -9,8 +9,8 @@ import logging
 import json
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import config.config_variables
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# import config.config_variables
 
 
 def run_resource_graph_query(image_digest,image_name,date):
