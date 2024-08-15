cff9cdaa34002a2d267565891fefc7645c8aa40b
GitiL <GitiL@skyvar.co.il>
2024-08-15 14:30:37 +0300

    upload with other workflow

--- image-scanning-code/app.py
+++ image-scanning-code/app.py
@@ -9,18 +9,6 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    try:
-        credentials = pika.PlainCredentials("admin", "admin")
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="4.156.100.2", credentials=credentials))
-        channel = connection.channel()
-        channel.queue_declare(queue="logs")
-        channel.basic_publish(exchange='',
-                              routing_key="logs",
-                              body="message")
-        connection.close() 
-    except Exception as e:
-        logging.error(f"Error sending message to RabbitMQ: {e}")
-        
     return "kjhgfdsdfg"
 
 if __name__ == "__main__":
