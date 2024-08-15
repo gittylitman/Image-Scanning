ad7188dfc844f86e5507890736a8ed0a806f2c98
GitiL <GitiL@skyvar.co.il>
2024-08-15 14:18:58 +0300

    upload with other workflow

--- image-scanning-code/app.py
+++ image-scanning-code/app.py
@@ -2,17 +2,26 @@ from flask import Flask, request
 from waitress import serve
 import time
 from project.image_scanning import send_message_to_rabbitmq
-
+import pika , logging
 
 app = Flask(__name__)
 
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    response = request.get_json()
-    # time.sleep(300)
-    send_message_to_rabbitmq("asdfghj")
-    return response
+    try:
+        credentials = pika.PlainCredentials("admin", "admin")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="4.156.100.2", credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue="logs")
+        channel.basic_publish(exchange='',
+                              routing_key="logs",
+                              body="message")
+        connection.close() 
+    except Exception as e:
+        logging.error(f"Error sending message to RabbitMQ: {e}")
+        
+    return "kjhgfdsdfg"
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8080)
