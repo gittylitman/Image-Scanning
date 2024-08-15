380df0393da3573e645ae91c758a0d1ba234e37b
GitiL <GitiL@skyvar.co.il>
2024-08-15 13:15:50 +0300

    upload with other workflow

--- image-scanning-code/app.py
+++ image-scanning-code/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from waitress import serve
 import time
-from project.image_scanning import run_resource_graph_query
+from project.image_scanning import run_resource_graph_query, send_message_to_rabbitmq
 
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def send_to_image_scanning():
     response = request.get_json()
     # time.sleep(300)
-    run_resource_graph_query(response["target"]["repository"])
+    send_message_to_rabbitmq(response["target"]["repository"])
     return response
 
 if __name__ == "__main__":
